69e04b4a1590cd0b6b0cb29756cc6b1bebc4f763
amazingbutuseless <cindian@amazingbutuseless.net>
2021-05-24 03:16:27 +0900

    favicon 추가

--- src/html.js
+++ src/html.js
@@ -33,6 +33,11 @@ export default function HTML(props) {
           href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="shortcut icon"
+          href="/static/favicon.ico"
+          type="image/x-icon"
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} css={bodyStyle}>
